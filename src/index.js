209802302459073f739cc60bf83651b4b74c9262
Adrian le Bas <adrianleb@gmail.com>
2019-11-06 20:06:59 +0100

    use a subprovider, bump version

--- src/index.js
+++ src/index.js
@@ -1,61 +1,25 @@
 import WalletConnect from '@walletconnect/browser';
 import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal';
+import WalletConnectSubprovider from "@walletconnect/web3-subprovider";
 
 export default function(maker) {
   const WALLETCONNECT = 'walletconnect';
-
   maker
     .service('accounts', true)
     .addAccountType(WALLETCONNECT, async settings => {
-      const walletConnectProvider = new WalletConnect({
+      const walletConnectProvider = new WalletConnectSubprovider({
         bridge: 'https://bridge.walletconnect.org'
       });
-
-      // setEngine and handleRequest are expected by the web3ProviderEngine
-      function setEngine(engine) {
-        const self = this;
-        if (self.engine) return;
-        self.engine = engine;
-        engine.on('block', function(block) {
-          self.currentBlock = block;
-        });
-
-        engine.on('start', function() {
-          self.start();
-        });
-
-        engine.on('stop', function() {
-          self.stop();
-        });
-      }
-
-      function handleRequest(payload, next, end) {
-        const self = this;
-
-        self
-          .sendTransaction(payload, (err, result) => {
-            return result ? end(null, result.result) : end(err);
-          })
-          .then(result => result)
-          .catch(err => err);
-      }
-
-      walletConnectProvider.setEngine = setEngine;
-      walletConnectProvider.handleRequest = handleRequest;
 
 
       // Check if connection is already established
       if (!walletConnectProvider.connected) {
-        console.log('not connected');
         // create new session
         walletConnectProvider.createSession().then(() => {
-          console.log('yoo');
           // get uri for QR Code modal
           const uri = walletConnectProvider.uri;
-          console.log('walletConnect uri', uri);
           // display QR Code modal
           WalletConnectQRCodeModal.open(uri, () => {
-            console.log('QR Code Modal closed');
           });
         });
       } else {
@@ -84,7 +48,6 @@ export default function(maker) {
         // Subscribe to connection events
         walletConnectProvider.on('connect', (error, payload) => {
           if (error) {
-            console.log('ERROR connecting to WALLET CONNECT:', error);
             throw error;
           }
           clearTimeout(fail);
